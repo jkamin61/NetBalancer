4f26feb4e739dc3cd2e28a20b03274da07ddad2c
Juliusz Kamiński <107152863+jkamin61@users.noreply.github.com>
2024-12-06 00:03:49 +0100

    Master class initialization + getting buffer ready to receive data

--- src/main/java/org/net_balancer/Master.java
+++ src/main/java/org/net_balancer/Master.java
@@ -1,4 +1,28 @@
 package org.net_balancer;
 
+import java.net.DatagramPacket;
+import java.net.DatagramSocket;
+import java.util.ArrayList;
+import java.util.List;
+
 public class Master {
+    private final DatagramSocket socket;
+    private final int initialNumber;
+    private final List<Integer> receivedNumbers;
+
+    public Master(DatagramSocket socket, int initialNumber) {
+        this.socket = socket;
+        this.initialNumber = initialNumber;
+        this.receivedNumbers = new ArrayList<>();
+        this.receivedNumbers.add(initialNumber);
+    }
+
+    public void run() {
+        try {
+            byte[] buffer = new byte[256];
+            DatagramPacket packet = new DatagramPacket(buffer, buffer.length);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
 }
