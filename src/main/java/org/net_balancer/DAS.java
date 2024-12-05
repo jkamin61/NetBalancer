eb6557799df3ccd938d10baa8527a4d694c4c93b
Juliusz Kamiński <107152863+jkamin61@users.noreply.github.com>
2024-12-05 23:12:14 +0100

    checking if there are two args <port> <number>, if not exiting

--- src/main/java/org/net_balancer/DAS.java
+++ src/main/java/org/net_balancer/DAS.java
@@ -2,6 +2,9 @@ package org.net_balancer;
 
 public class DAS {
     public static void main(String[] args) {
-
+        if (args.length != 2) {
+            System.err.println("Usage: java DAS <port> <number>");
+            System.exit(1);
+        }
     }
 }
