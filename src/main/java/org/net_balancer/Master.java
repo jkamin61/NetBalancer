1d709df000f54e61e02dff530543da5cd6aac64b
Juliusz Kamiński <107152863+jkamin61@users.noreply.github.com>
2024-12-06 20:38:33 +0100

    handling when input value is 0 -> calculating average from already received values and printing it

--- src/main/java/org/net_balancer/Master.java
+++ src/main/java/org/net_balancer/Master.java
@@ -1,5 +1,6 @@
 package org.net_balancer;
 
+import java.io.IOException;
 import java.net.DatagramPacket;
 import java.net.DatagramSocket;
 import java.util.ArrayList;
@@ -20,9 +21,28 @@ public class Master {
     public void run() {
         try {
             byte[] buffer = new byte[256];
-            DatagramPacket packet = new DatagramPacket(buffer, buffer.length);
+            while (true) {
+                DatagramPacket packet = new DatagramPacket(buffer, buffer.length);
+                socket.receive(packet);
+
+                String message = new String(packet.getData(), 0, packet.getLength());
+                int receivedNumber = Integer.parseInt(message);
+
+                if (receivedNumber == 0) {
+                    broadcastAverage();
+                }
+            }
         } catch (Exception e) {
             e.printStackTrace();
         }
+    }
+
+    private void broadcastAverage() throws IOException {
+        double average = receivedNumbers.stream()
+                .filter(num -> num != 0 && num != -1)
+                .mapToInt(Integer::intValue)
+                .average()
+                .orElse(0.0);
+        System.out.println("Average: " + average);
     }
 }
