0af88880ee75977f0cc85009b071e5d03df64924
Juliusz Kamiński <107152863+jkamin61@users.noreply.github.com>
2024-12-06 21:38:18 +0100

    Slave mode implemented with run method to send data on master port

--- src/main/java/org/net_balancer/Slave.java
+++ src/main/java/org/net_balancer/Slave.java
@@ -1,4 +1,33 @@
 package org.net_balancer;
 
+import java.net.DatagramPacket;
+import java.net.DatagramSocket;
+import java.net.InetAddress;
+
 public class Slave {
+    private final int masterPort;
+    private final int number;
+
+    public Slave(int masterPort, int number) {
+        this.masterPort = masterPort;
+        this.number = number;
+    }
+
+    public void run() {
+        try (DatagramSocket socket = new DatagramSocket()) {
+            InetAddress localHost = InetAddress.getByName("localhost");
+            String message = String.valueOf(number);
+
+            DatagramPacket packet = new DatagramPacket(
+                    message.getBytes(),
+                    message.length(),
+                    localHost,
+                    masterPort
+            );
+            socket.send(packet);
+            System.out.println("Sent number " + number + " to master on port " + masterPort);
+        } catch (Exception e) {
+            System.err.println("Error in Slave: " + e.getMessage());
+        }
+    }
 }
